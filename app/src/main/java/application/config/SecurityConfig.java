4b036b2006c49fc5293e73928cd11fcb7c2268ce
Jonas Godoi <jetosbaldo91@hotmail.com>
2024-09-03 18:44:46 +0000

    Commit-03092024

--- app/src/main/java/application/config/SecurityConfig.java
+++ app/src/main/java/application/config/SecurityConfig.java
@@ -9,7 +9,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -38,10 +39,9 @@ public class SecurityConfig {
         AuthenticationConfiguration configuration) throws Exception {
             return configuration.getAuthenticationManager();
     }
-
-    @SuppressWarnings("deprecation")
+    
     @Bean
-    public NoOpPasswordEncoder passwordEncoder() {
-        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
